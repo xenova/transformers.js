c7324f7f4e90a6abe267070f39335c88e4e6e24a
Joshua Lochner <admin@xenova.com>
2023-02-21 16:51:42 +0200

    Set wasm path in transformer.js

--- src/transformers.js
+++ src/transformers.js
@@ -20,8 +20,16 @@ if (typeof window === 'undefined') {
 
 } else {
     // We are running in the main thread
-    // Allow global access to these variables
 
+    // First, we set the wasm path to point to the directory which contains this file.
+    // https://stackoverflow.com/a/42594856
+    let scriptPath = (() => {
+        return new Error().stack.match(/([^ \n])*([a-z]*:\/\/\/?)*?[a-z0-9\/\\]*\.js/ig)[0]
+    })();
+    let scriptFolder = (new URL(scriptPath)).pathname;
+    ort.env.wasm.wasmPaths = scriptFolder.substring(0, scriptFolder.lastIndexOf('/') + 1);
+
+    // Allow global access to these variables
     window.AutoTokenizer = AutoTokenizer
     window.BertTokenizer = BertTokenizer
     window.DistilBertTokenizer = DistilBertTokenizer
