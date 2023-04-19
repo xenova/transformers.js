a3ed4e3f185a38291ace38e7e1a6210a4f8be548
Joshua Lochner <admin@xenova.com>
2023-04-19 17:48:00 +0200

    Update `GenerationConfig` documentation

--- src/generation.js
+++ src/generation.js
@@ -375,40 +375,68 @@ class RepetitionPenaltyLogitsProcessor extends LogitsProcessor {
 
 
 class GenerationConfig {
+    /**
+     * Create a GenerationConfig object
+     * @constructor
+     * @param {Object} [kwargs={}] - The configuration parameters. If not set, the default values are used.
+     * 
+     * @param {number} [kwargs.max_length=20] The maximum length the generated tokens can have. Corresponds to the length of the input prompt + `max_new_tokens`. Its effect is overridden by `max_new_tokens`, if also set.
+     * @param {number} [kwargs.max_new_tokens=null] The maximum numbers of tokens to generate, ignoring the number of tokens in the prompt.
+     * @param {number} [kwargs.min_length=0] The minimum length of the sequence to be generated. Corresponds to the length of the input prompt + `min_new_tokens`. Its effect is overridden by `min_new_tokens`, if also set.
+     * @param {number} [kwargs.min_new_tokens=null] The minimum numbers of tokens to generate, ignoring the number of tokens in the prompt.
+     * @param {boolean|"never"} [kwargs.early_stopping=false] Controls the stopping condition for beam-based methods, like beam-search. It accepts the following values:
+     * - `true`, where the generation stops as soon as there are `num_beams` complete candidates;
+     * - `false`, where an heuristic is applied and the generation stops when is it very unlikely to find better candidates;
+     * - `"never"`, where the beam search procedure only stops when there cannot be better candidates (canonical beam search algorithm).
+     * @param {number} [kwargs.max_time=null] The maximum amount of time you allow the computation to run for in seconds. Generation will still finish the current pass after allocated time has been passed.
+     *
+     * @param {boolean} [kwargs.do_sample=false] Whether or not to use sampling; use greedy decoding otherwise.
+     * @param {number} [kwargs.num_beams=1] Number of beams for beam search. 1 means no beam search.
+     * @param {number} [kwargs.num_beam_groups=1] Number of groups to divide `num_beams` into in order to ensure diversity among different groups of beams. See [this paper](https://arxiv.org/pdf/1610.02424.pdf) for more details.
+     * @param {number} [kwargs.penalty_alpha=null] The values balance the model confidence and the degeneration penalty in contrastive search decoding.
+     * @param {boolean} [kwargs.use_cache=true] Whether or not the model should use the past last key/values attentions (if applicable to the model) to speed up decoding.
+     *
+     * @param {number} [kwargs.temperature=1.0] The value used to modulate the next token probabilities.
+     * @param {number} [kwargs.top_k=50] The number of highest probability vocabulary tokens to keep for top-k-filtering.
+     * @param {number} [kwargs.top_p=1.0] If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to `top_p` or higher are kept for generation.
+     * @param {number} [kwargs.typical_p=1.0] Local typicality measures how similar the conditional probability of predicting a target token next is to the expected conditional probability of predicting a random token next, given the partial text already generated. If set to float < 1, the smallest set of the most locally typical tokens with probabilities that add up to `typical_p` or higher are kept for generation. See [this paper](https://arxiv.org/pdf/2202.00666.pdf) for more details.
+     * @param {number} [kwargs.epsilon_cutoff=0.0] If set to float strictly between 0 and 1, only tokens with a conditional probability greater than `epsilon_cutoff` will be sampled. In the paper, suggested values range from 3e-4 to 9e-4, depending on the size of the model. See [Truncation Sampling as Language Model Desmoothing](https://arxiv.org/abs/2210.15191) for more details.
+     * @param {number} [kwargs.eta_cutoff=0.0] Eta sampling is a hybrid of locally typical sampling and epsilon sampling. If set to float strictly between 0 and 1, a token is only considered if it is greater than either `eta_cutoff` or `sqrt(eta_cutoff) * exp(-entropy(softmax(next_token_logits)))`. The latter term is intuitively the expected next token probability, scaled by `sqrt(eta_cutoff)`. In the paper, suggested values range from 3e-4 to 2e-3, depending on the size of the model. See [Truncation Sampling as Language Model Desmoothing](https://arxiv.org/abs/2210.15191) for more details.
+     * @param {number} [kwargs.diversity_penalty=0.0] This value is subtracted from a beam's score if it generates a token same as any beam from other group at a particular time. Note that `diversity_penalty` is only effective if `group beam search` is enabled.
+     * @param {number} [kwargs.repetition_penalty=1.0] The parameter for repetition penalty. 1.0 means no penalty. See [this paper](https://arxiv.org/pdf/1909.05858.pdf) for more details.
+     * @param {number} [kwargs.encoder_repetition_penalty=1.0] The paramater for encoder_repetition_penalty. An exponential penalty on sequences that are not in the original input. 1.0 means no penalty.
+     * @param {number} [kwargs.length_penalty=1.0] Exponential penalty to the length that is used with beam-based generation. It is applied as an exponent to the sequence length, which in turn is used to divide the score of the sequence. Since the score is the log likelihood of the sequence (i.e. negative), `length_penalty` > 0.0 promotes longer sequences, while `length_penalty` < 0.0 encourages shorter sequences.
+     * @param {number} [kwargs.no_repeat_ngram_size=0] If set to int > 0, all ngrams of that size can only occur once.
+     * @param {number[][]} [kwargs.bad_words_ids=null] List of token ids that are not allowed to be generated. In order to get the token ids of the words that should not appear in the generated text, use `(await tokenizer(bad_words, {add_prefix_space: true, add_special_tokens: false})).input_ids`.
+     * @param {number[][]|number[][][]} [kwargs.force_words_ids=null] List of token ids that must be generated. If given a `number[][]`, this is treated as a simple list of words that must be included, the opposite to `bad_words_ids`. If given `number[][][]`, this triggers a [disjunctive constraint](https://github.com/huggingface/transformers/issues/14081), where one can allow different forms of each word.
+     * @param {boolean} [kwargs.renormalize_logits=false] Whether to renormalize the logits after applying all the logits processors or warpers (including the custom ones). It's highly recommended to set this flag to `true` as the search algorithms suppose the score logits are normalized but some logit processors or warpers break the normalization.
+     * @param {Object[]} [kwargs.constraints=null] Custom constraints that can be added to the generation to ensure that the output will contain the use of certain tokens as defined by `Constraint` objects, in the most sensible way possible.
+     * 
+     * @param {number} [kwargs.forced_bos_token_id=null] The id of the token to force as the first generated token after the `decoder_start_token_id`. Useful for multilingual models like mBART where the first generated token needs to be the target language token.
+     * @param {number|number[]} [kwargs.forced_eos_token_id=null] The id of the token to force as the last generated token when `max_length` is reached. Optionally, use a list to set multiple *end-of-sequence* tokens.
+     * @param {boolean} [kwargs.remove_invalid_values=false] Whether to remove possible *nan* and *inf* outputs of the model to prevent the generation method to crash. Note that using `remove_invalid_values` can slow down generation.
+     * @param {number[]} [kwargs.exponential_decay_length_penalty=null] This Tuple adds an exponentially increasing length penalty, after a certain amount of tokens have been generated. The tuple shall consist of: `(start_index, decay_factor)` where `start_index` indicates where penalty starts and `decay_factor` represents the factor of exponential decay.
+     * @param {number[]} [kwargs.suppress_tokens=null] A list of tokens that will be suppressed at generation. The `SupressTokens` logit processor will set their log probs to `-inf` so that they are not sampled.
+     * @param {number[]} [kwargs.begin_suppress_tokens=null] A list of tokens that will be suppressed at the beginning of the generation. The `SupressBeginTokens` logit processor will set their log probs to `-inf` so that they are not sampled.
+     * @param {number[][]} [kwargs.forced_decoder_ids=null] A list of pairs of integers which indicates a mapping from generation indices to token indices that will be forced before sampling. For example, `[[1, 123]]` means the second generated token will always be a token of index 123.
+     * 
+     * @param {number} [kwargs.num_return_sequences=1] The number of independently computed returned sequences for each element in the batch.
+     * @param {boolean} [kwargs.output_attentions=false] Whether or not to return the attentions tensors of all attention layers. See `attentions` under returned tensors for more details.
+     * @param {boolean} [kwargs.output_hidden_states=false] Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors for more details.
+     * @param {boolean} [kwargs.output_scores=false] Whether or not to return the prediction scores. See `scores` under returned tensors for more details.
+     * @param {boolean} [kwargs.return_dict_in_generate=false] Whether or not to return a `ModelOutput` instead of a plain tuple.
+     * 
+     * @param {number} [kwargs.pad_token_id=null] The id of the *padding* token.
+     * @param {number} [kwargs.bos_token_id=null] The id of the *beginning-of-sequence* token.
+     * @param {number|number[]} [kwargs.eos_token_id=null] The id of the *end-of-sequence* token. Optionally, use a list to set multiple *end-of-sequence* tokens.
+     * 
+     * @param {number} [kwargs.encoder_no_repeat_ngram_size=0] If set to int > 0, all ngrams of that size that occur in the `encoder_input_ids` cannot occur in the `decoder_input_ids`.
+     * @param {number} [kwargs.decoder_start_token_id=null] If an encoder-decoder model starts decoding with a different token than *bos*, the id of that token.
+     * 
+     * @param {Object} [kwargs.generation_kwargs={}] Additional generation kwargs will be forwarded to the `generate` function of the model. Kwargs that are not present in `generate`'s signature will be used in the model forward pass.
+     */
     constructor(kwargs = {}) {
         // Parameters that control the length of the output
-        // TODO: extend the configuration with correct types
-        /**
-         * Create a GenerationConfig object
-         * @constructor
-         * @param {Object} [kwargs={}] - The configuration parameters
-         * @param {number} [kwargs.max_length=20] - The maximum length of the generated text
-         * @param {number} [kwargs.max_new_tokens=null] - The maximum number of new tokens to generate
-         * @param {number} [kwargs.min_length=0] - The minimum length of the generated text
-         * @param {number} [kwargs.min_new_tokens=null] - The minimum number of new tokens to generate
-         * @param {boolean} [kwargs.early_stopping=false] - Whether to stop generation early if a stop token is encountered
-         * @param {number} [kwargs.max_time=null] - The maximum amount of time to spend generating text
-         * @param {boolean} [kwargs.do_sample=false] - Whether to use sampling when generating text
-         * @param {number} [kwargs.num_beams=1] - The number of beams to use when generating text
-         * @param {number} [kwargs.num_beam_groups=1] - The number of beam groups to use when generating text
-         * @param {number} [kwargs.penalty_alpha=null] - The value of the alpha penalty to use when generating text
-         * @param {boolean} [kwargs.use_cache=true] - Whether to use cache when generating text
-         * @param {number} [kwargs.temperature=1.0] - The temperature to use when generating text
-         * @param {number} [kwargs.top_k=50] - The value of k to use when generating text
-         * @param {number} [kwargs.top_p=1.0] - The value of p to use when generating text
-         * @param {number} [kwargs.typical_p=1.0] - The typical value of p to use when generating text
-         * @param {number} [kwargs.epsilon_cutoff=0.0] - The value of epsilon cutoff to use when generating text
-         * @param {number} [kwargs.eta_cutoff=0.0] - The value of eta cutoff to use when generating text
-         * @param {number} [kwargs.diversity_penalty=0.0] - The value of diversity penalty to use when generating text
-         * @param {number} [kwargs.repetition_penalty=1.0] - The value of repetition penalty to use when generating text
-         * @param {number} [kwargs.encoder_repetition_penalty=1.0] - The value of encoder repetition penalty to use when generating text
-         * @param {number} [kwargs.length_penalty=1.0] - The value of length
-         * @param {number} [kwargs.no_repeat_ngram_size=0] - The size of the n-grams to avoid repeating in the generated output.
-         * @param {?number[]} [kwargs.bad_words_ids=null] - An array of IDs representing tokens that should not be generated.
-         * @param {?number[]} [kwargs.force_words_ids=null] - An array of IDs representing tokens that must be generated.
-         * @param {boolean} [kwargs.renormalize_logits=false] - Whether or not to renormalize the logits before generating new tokens.
-         * @param {?Object[]} [kwargs.constraints=null] - An array of constraint objects to apply during generation.
-         */
         this.max_length = kwargs.max_length ?? 20;
         this.max_new_tokens = kwargs.max_new_tokens ?? null;
         this.min_length = kwargs.min_length ?? 0;
