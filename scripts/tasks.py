0dcae74cc7b6e10761cff6925c9b7cfa09162b6c
Joshua Lochner <admin@xenova.com>
2023-04-21 23:22:00 +0200

    Update tasks.py to use new model format

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -28,299 +28,113 @@ SUPPORTED_TASKS = {
 
 SUPPORTED_MODELS = {
     'albert': {
-        'albert-base-v2': [
-            'default',
-            'masked-lm',
-        ],
-        'albert-large-v2': [
-            'default',
-            'masked-lm',
-        ],
-        'sentence-transformers/paraphrase-albert-small-v2': [
-            'default',
-        ],
-        'sentence-transformers/paraphrase-albert-base-v2': [
-            'default',
-        ],
+        'albert-base-v2',
+        'albert-large-v2',
+        'sentence-transformers/paraphrase-albert-small-v2',
+        'sentence-transformers/paraphrase-albert-base-v2',
     },
+
     'bart': {
-        'sshleifer/distilbart-cnn-6-6': [
-            'seq2seq-lm-with-past'
-        ],
-        'facebook/bart-large-cnn': [
-            'seq2seq-lm-with-past'
-        ],
-        'facebook/bart-large-mnli': [
-            'default',
-            'sequence-classification',
-        ],
+        'sshleifer/distilbart-cnn-6-6',
+        'facebook/bart-large-cnn',
+        'facebook/bart-large-mnli',
     },
     'bert': {
-        'bert-base-uncased': [
-            'default',
-            'masked-lm',
-        ],
-        'bert-base-cased': [
-            'default',
-            'masked-lm',
-        ],
-        'bert-base-multilingual-uncased': [
-            'default',
-            'masked-lm',
-        ],
-        'bert-base-multilingual-cased': [
-            'default',
-            'masked-lm',
-        ],
-        'nlptown/bert-base-multilingual-uncased-sentiment': [
-            'sequence-classification',
-        ],
-        'sentence-transformers/all-MiniLM-L6-v2': [
-            'default',
-        ],
-        'sentence-transformers/all-MiniLM-L12-v2': [
-            'default',
-        ],
+        'bert-base-uncased',
+        'bert-base-cased',
+        'bert-base-multilingual-uncased',
+        'bert-base-multilingual-cased',
+        'nlptown/bert-base-multilingual-uncased-sentiment',
+        'sentence-transformers/all-MiniLM-L6-v2',
+        'sentence-transformers/all-MiniLM-L12-v2',
 
-
-        'Davlan/bert-base-multilingual-cased-ner-hrl': [
-            # 'default',
-            'token-classification',
-        ],
-        'ckiplab/bert-base-chinese-ner': [
-            # 'default',
-            'token-classification',
-        ],
-        'ckiplab/bert-base-chinese-ws': [
-            # 'default',
-            'token-classification',
-        ],
-        'ckiplab/bert-base-chinese-pos': [
-            # 'default',
-            'token-classification',
-        ],
-        'dslim/bert-base-NER': [
-            # 'default',
-            'token-classification',
-        ],
-        'dslim/bert-base-NER-uncased': [
-            # 'default',
-            'token-classification',
-        ],
+        'Davlan/bert-base-multilingual-cased-ner-hrl',
+        'ckiplab/bert-base-chinese-ner',
+        'ckiplab/bert-base-chinese-ws',
+        'ckiplab/bert-base-chinese-pos',
+        'dslim/bert-base-NER',
+        'dslim/bert-base-NER-uncased',
     },
     'blenderbot-small': {
-        'facebook/blenderbot_small-90M': [
-            'default',
-            'causal-lm-with-past',
-            'seq2seq-lm-with-past',
-        ]
+        'facebook/blenderbot_small-90M',
     },
     'clip': {
-        'openai/clip-vit-base-patch16': [
-            'default'
-        ],
-        'openai/clip-vit-base-patch32': [
-            'default'
-        ],
+        'openai/clip-vit-base-patch16',
+        'openai/clip-vit-base-patch32',
     },
     'codegen': {
-        'Salesforce/codegen-350M-mono': [
-            'default',
-            'causal-lm-with-past',
-        ],
-        'Salesforce/codegen-350M-multi': [
-            'default',
-            'causal-lm-with-past',
-        ],
-        'Salesforce/codegen-350M-nl': [
-            'default',
-            'causal-lm-with-past',
-        ],
+        'Salesforce/codegen-350M-mono',
+        'Salesforce/codegen-350M-multi',
+        'Salesforce/codegen-350M-nl',
     },
     'detr': {
-        'facebook/detr-resnet-50': [
-            'default',
-            'object-detection',
-            # 'image-segmentation',
-        ],
-        'facebook/detr-resnet-101': [
-            'default',
-            'object-detection',
-            # 'image-segmentation',
-        ],
+        'facebook/detr-resnet-50',
+        'facebook/detr-resnet-101',
 
-        'facebook/detr-resnet-50-panoptic': [
-            'image-segmentation',
-        ],
-        
+        'facebook/detr-resnet-50-panoptic',
+
     },
     'distilbert': {
-        'distilbert-base-uncased': [
-            'default',
-            'masked-lm',
-        ],
-        'distilbert-base-cased': [
-            'default',
-            'masked-lm',
-        ],
-        'distilbert-base-uncased-distilled-squad': [
-            'default',
-            'question-answering'
-        ],
-        'distilbert-base-cased-distilled-squad': [
-            'default',
-            'question-answering'
-        ],
-        'distilbert-base-uncased-finetuned-sst-2-english': [
-            'sequence-classification',
-        ],
-        'typeform/distilbert-base-uncased-mnli': [
-            'default',
-            'sequence-classification',
-        ],
+        'distilbert-base-uncased',
+        'distilbert-base-cased',
+        'distilbert-base-uncased-distilled-squad',
+        'distilbert-base-cased-distilled-squad',
+        'distilbert-base-uncased-finetuned-sst-2-english',
+        'typeform/distilbert-base-uncased-mnli',
 
-        'Davlan/distilbert-base-multilingual-cased-ner-hrl': [
-            # 'default',
-            'token-classification',
-        ],
+        'Davlan/distilbert-base-multilingual-cased-ner-hrl',
     },
     'gpt-neo': {
-        'EleutherAI/gpt-neo-125M': [
-            'default',
-            'causal-lm-with-past',
-        ],
+        'EleutherAI/gpt-neo-125M',
     },
     'gpt2': {
-        'gpt2': [
-            'default',
-            'causal-lm-with-past',
-        ],
-        'distilgpt2': [
-            'default',
-            'causal-lm-with-past',
-            'sequence-classification',
-            'token-classification',
-        ]
+        'gpt2',
+        'distilgpt2',
     },
     'marian': {
-        f'Helsinki-NLP/opus-mt-{x}': [
-            'default',
-            'seq2seq-lm-with-past',
-        ]
+        f'Helsinki-NLP/opus-mt-{x}'
         for x in SUPPORTED_HELSINKI_NLP_MODELS
     },
     'mobilebert': {
-        'google/mobilebert-uncased': [
-            'default',
-            'masked-lm',
-        ],
-        'typeform/mobilebert-uncased-mnli': [
-            'default',
-            'sequence-classification',
-        ],
+        'google/mobilebert-uncased',
+        'typeform/mobilebert-uncased-mnli',
     },
     'mt5': {
-        'google/mt5-small': [
-            'default',
-            # 'seq2seq-lm-with-past',
-        ]
+        'google/mt5-small',
     },
     'roberta': {
-        'xlm-roberta-base': [
-            'default',
-            'masked-lm',
-        ], 'roberta-base': [
-            'default',
-            'masked-lm',
-        ],
-        'distilroberta-base': [
-            'default',
-            'masked-lm',
-        ],
-        'sentence-transformers/all-distilroberta-v1': [
-            'default'
-        ],
-        'roberta-large-mnli': [
-            'default',
-            'sequence-classification',
-        ]
+        'xlm-roberta-base',
+        'roberta-base',
+        'distilroberta-base',
+        'sentence-transformers/all-distilroberta-v1',
+        'roberta-large-mnli',
     },
     'squeezebert': {
-        'squeezebert/squeezebert-uncased': [
-            'default',
-            'masked-lm',
-        ],
-        'squeezebert/squeezebert-mnli': [
-            'default',
-            'sequence-classification',
-        ]
+        'squeezebert/squeezebert-uncased',
+        'squeezebert/squeezebert-mnli',
     },
     't5': {
-        't5-small': [
-            'default',
-            'seq2seq-lm-with-past',
-        ],
-        't5-base': [
-            'default',
-            'seq2seq-lm-with-past',
-        ],
-        'google/t5-v1_1-small': [
-            'default',
-            'seq2seq-lm-with-past',
-        ],
-        'google/t5-v1_1-base': [
-            'default',
-            'seq2seq-lm-with-past',
-        ],
-        'google/flan-t5-small': [
-            'default',
-            'seq2seq-lm-with-past',
-        ],
-        'google/flan-t5-base': [
-            'default',
-            'seq2seq-lm-with-past',
-        ]
+        't5-small',
+        't5-base',
+        'google/t5-v1_1-small',
+        'google/t5-v1_1-base',
+        'google/flan-t5-small',
+        'google/flan-t5-base',
     },
     'vision-encoder-decoder': {
-        'nlpconnect/vit-gpt2-image-captioning': [
-            # 'vision2seq-lm',
-            'vision2seq-lm-with-past',
-        ]
+        'nlpconnect/vit-gpt2-image-captioning',
     },
     'vit': {
-        'google/vit-base-patch16-224-in21k': [
-            'default'
-        ],
-        'google/vit-base-patch16-224': [
-            'default',
-            'image-classification'
-        ]
+        'google/vit-base-patch16-224-in21k',
+        'google/vit-base-patch16-224',
     },
     'whisper': {
-        'openai/whisper-tiny': [
-            'default',
-            'speech2seq-lm-with-past'
-        ],
-        'openai/whisper-tiny.en': [
-            'default',
-            'speech2seq-lm-with-past'
-        ],
-        'openai/whisper-base': [
-            'default',
-            'speech2seq-lm-with-past'
-        ],
-        'openai/whisper-base.en': [
-            'default',
-            'speech2seq-lm-with-past'
-        ],
-        'openai/whisper-small': [
-            'default',
-            'speech2seq-lm-with-past'
-        ],
-        'openai/whisper-small.en': [
-            'default',
-            'speech2seq-lm-with-past'
-        ],
+        'openai/whisper-tiny',
+        'openai/whisper-tiny.en',
+        'openai/whisper-base',
+        'openai/whisper-base.en',
+        'openai/whisper-small',
+        'openai/whisper-small.en',
     },
 }
 
@@ -328,10 +142,9 @@ SUPPORTED_MODELS = {
 def main():
     for model_type, model_ids in SUPPORTED_MODELS.items():
         print(f'# {model_type:=^80}')
-        for model_id, tasks in model_ids.items():
-            for task in tasks:
-                print(
-                    f'python -m scripts.convert --model_id {model_id} --from_hub --quantize --task {task}')
+        for model_id in model_ids:
+            print(
+                f'python -m scripts.convert --quantize --model_id {model_id}')
         print()
 
 
