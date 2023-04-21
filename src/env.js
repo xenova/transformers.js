9a4b9c600606a533fed7bd286eedfcdd54006c2b
Joshua Lochner <admin@xenova.com>
2023-04-21 02:46:45 +0200

    Update available environment variables

--- src/env.js
+++ src/env.js
@@ -3,18 +3,21 @@ const path = require('path');
 
 const { env: onnx_env } = require('./backends/onnx.js').ONNX;
 
+// Only used for environments with access to file system
+const DEFAULT_CACHE_DIR = path.join(path.dirname(__dirname), '/.cache/')
+
 // check if various APIs are available (depends on environment)
-const CACHE_AVAILABLE = typeof self !== 'undefined' && 'caches' in self;
+const WEB_CACHE_AVAILABLE = typeof self !== 'undefined' && 'caches' in self;
 const FS_AVAILABLE = !isEmpty(fs); // check if file system is available
 const PATH_AVAILABLE = !isEmpty(path); // check if path is available
 
 const RUNNING_LOCALLY = FS_AVAILABLE && PATH_AVAILABLE;
 
 // set local model path, based on available APIs
-const DEFAULT_LOCAL_PATH = '/models/onnx/quantized/';
-const localURL = RUNNING_LOCALLY
-    ? path.join(path.dirname(__dirname), DEFAULT_LOCAL_PATH)
-    : DEFAULT_LOCAL_PATH;
+const DEFAULT_LOCAL_MODEL_PATH = '/models/';
+const localModelPath = RUNNING_LOCALLY
+    ? path.join(path.dirname(__dirname), DEFAULT_LOCAL_MODEL_PATH)
+    : DEFAULT_LOCAL_MODEL_PATH;
 
 // First, set path to wasm files. This is needed when running in a web worker.
 // https://onnxruntime.ai/docs/api/js/interfaces/Env.WebAssemblyFlags.html#wasmPaths
@@ -27,23 +30,40 @@ onnx_env.wasm.wasmPaths = RUNNING_LOCALLY
 
 // Global variable used to control exection, with suitable defaults
 const env = {
-    // access onnxruntime-web's environment variables
-    onnx: onnx_env,
+    // Expose environment variables of different backends, allowing users to set
+    // these variables if they want to.
+    // TODO - will be used when we add more backends
+    backends: {
+        // onnxruntime-web/onnxruntime-node
+        onnx: onnx_env,
 
-    // whether to support loading models from the HuggingFace hub
-    remoteModels: true,
+        // TensorFlow.js
+        tfjs: {},
+    },
 
-    // URL to load models from
-    remoteURL: 'https://huggingface.co/Xenova/transformers.js/resolve/main/quantized/',
+    // URL to load models from. Defaults to the Hugging Face Hub.
+    remoteHost: 'https://huggingface.co/',
+    remotePathTemplate: '{model}/resolve/{revision}/',
+
 
     // Local URL to load models from.
-    localURL: localURL,
-
-    // Whether to use Cache API to cache models. By default, it is true if available.
-    useCache: CACHE_AVAILABLE,
+    localModelPath: localModelPath,
 
     // Whether to use the file system to load files. By default, it is true available.
     useFS: FS_AVAILABLE,
+
+
+    /////////////////// Cache settings ///////////////////
+    // Whether to use Cache API to cache models. By default, it is true if available.
+    useBrowserCache: WEB_CACHE_AVAILABLE,
+
+    // Whether to use the file system to cache files. By default, it is true available.
+    useFSCache: FS_AVAILABLE,
+
+    // The directory to use for caching files with the file system. By default, it is `./.cache`.
+    cacheDir: DEFAULT_CACHE_DIR,
+    //////////////////////////////////////////////////////
+
 }
 
 
