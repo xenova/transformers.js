e8e4b5f2935c5102848e921efcefa4fd655c5a52
Joshua Lochner <admin@xenova.com>
2023-04-21 02:49:12 +0200

    Add typedef for `PretrainedOptions`

--- src/utils/hub.js
+++ src/utils/hub.js
@@ -7,6 +7,18 @@ const fs = require('fs');
 const { dispatchCallback } = require('../utils.js');
 const path = require('path');
 
+/**
+ * @typedef {object} PretrainedOptions Options for loading a pretrained model.     
+ * @property {boolean?} [options.quantized=true] - Whether to load the 8-bit quantized version of the model (only applicable when loading model files).
+ * @property {function} [options.progress_callback=null] If specified, this function will be called during model construction, to provide the user with progress updates.
+ * @property {object} [options.config=null] Configuration for the model to use instead of an automatically loaded configuration. Configuration can be automatically loaded when:
+ * - The model is a model provided by the library (loaded with the *model id* string of a pretrained model).
+ * - The model is loaded by supplying a local directory as `pretrained_model_name_or_path` and a configuration JSON file named *config.json* is found in the directory.
+ * @property {string} [options.cache_dir=null] Path to a directory in which a downloaded pretrained model configuration should be cached if the standard cache should not be used.
+ * @property {boolean} [options.local_files_only=false] Whether or not to only look at local files (e.g., not try downloading the model).
+ * @property {string} [options.revision='main'] The specific model version to use. It can be a branch name, a tag name, or a commit id,
+ * since we use a git-based system for storing models and other artifacts on huggingface.co, so `revision` can be any identifier allowed by git.
+ */
 
 if (global.ReadableStream === undefined && typeof process !== 'undefined') {
     try {
