926b279b7b2428b072ec6a7c8281dd71d32b80a1
Joshua Lochner <admin@xenova.com>
2023-05-01 01:16:32 +0200

    Support loading of audio sources with 1 channel

--- src/pipelines.js
+++ src/pipelines.js
@@ -707,30 +707,36 @@ class AutomaticSpeechRecognitionPipeline extends Pipeline {
             const decoded = await audioCTX.decodeAudioData(response);
 
             // We now replicate HuggingFace's `ffmpeg_read` method:
-            // 
-            // When downmixing a stereo audio file to mono using the -ac 1 option in FFmpeg,
-            // the audio signal is summed across both channels to create a single mono channel.
-            // However, if the audio is at full scale (i.e. the highest possible volume level),
-            // the summing of the two channels can cause the audio signal to clip or distort.
 
-            // To prevent this clipping, FFmpeg applies a scaling factor of 1/sqrt(2) (~ 0.707)
-            // to the audio signal before summing the two channels. This scaling factor ensures
-            // that the combined audio signal will not exceed the maximum possible level, even
-            // if both channels are at full scale.
+            if (decoded.numberOfChannels === 2) {
+                // When downmixing a stereo audio file to mono using the -ac 1 option in FFmpeg,
+                // the audio signal is summed across both channels to create a single mono channel.
+                // However, if the audio is at full scale (i.e. the highest possible volume level),
+                // the summing of the two channels can cause the audio signal to clip or distort.
 
-            // After applying this scaling factor, the audio signal from both channels is summed
-            // to create a single mono channel. It's worth noting that this scaling factor is
-            // only applied when downmixing stereo audio to mono using the -ac 1 option in FFmpeg.
-            // If you're using a different downmixing method, or if you're not downmixing the
-            // audio at all, this scaling factor may not be needed.
-            const SCALING_FACTOR = Math.sqrt(2);
+                // To prevent this clipping, FFmpeg applies a scaling factor of 1/sqrt(2) (~ 0.707)
+                // to the audio signal before summing the two channels. This scaling factor ensures
+                // that the combined audio signal will not exceed the maximum possible level, even
+                // if both channels are at full scale.
 
-            let left = decoded.getChannelData(0);
-            let right = decoded.getChannelData(1);
+                // After applying this scaling factor, the audio signal from both channels is summed
+                // to create a single mono channel. It's worth noting that this scaling factor is
+                // only applied when downmixing stereo audio to mono using the -ac 1 option in FFmpeg.
+                // If you're using a different downmixing method, or if you're not downmixing the
+                // audio at all, this scaling factor may not be needed.
+                const SCALING_FACTOR = Math.sqrt(2);
 
-            audio = new Float32Array(left.length);
-            for (let i = 0; i < decoded.length; i++) {
-                audio[i] = SCALING_FACTOR * (left[i] + right[i]) / 2;
+                let left = decoded.getChannelData(0);
+                let right = decoded.getChannelData(1);
+
+                audio = new Float32Array(left.length);
+                for (let i = 0; i < decoded.length; i++) {
+                    audio[i] = SCALING_FACTOR * (left[i] + right[i]) / 2;
+                }
+
+            } else {
+                // If the audio is not stereo, we can just use the first channel:
+                audio = decoded.getChannelData(0);
             }
         }
 
