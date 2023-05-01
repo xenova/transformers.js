459ac7f4f95a7566bdb25f687d14aaf845ea49ce
Joshua Lochner <admin@xenova.com>
2023-05-01 22:34:19 +0200

    Fix `BertPreTokenizer` regex pattern

--- src/tokenizers.js
+++ src/tokenizers.js
@@ -849,7 +849,7 @@ class BertPreTokenizer extends PreTokenizer {
     constructor(config) {
         super();
         // TODO use config
-        this.pattern = /\b\p{L}+\b|[^\s\p{L}]+/gu
+        this.pattern = /\b\p{L}+\b|[^\s\p{L}]/gu;
     }
     /**
      * Tokenizes a single text using the BERT pre-tokenization scheme.
