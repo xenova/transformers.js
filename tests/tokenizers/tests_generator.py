80912c0fb25dbad157dc116dc8dfd14751ae7953
Joshua Lochner <admin@xenova.com>
2023-05-02 05:10:07 +0200

    Reformat tests generator

--- tests/tokenizers/tests_generator.py
+++ tests/tokenizers/tests_generator.py
@@ -7,11 +7,11 @@ from transformers import AutoTokenizer
 TEST_DATA = {
     "albert": [
         "albert-base-v2",
-        # "sentence-transformers/paraphrase-albert-small-v2"
+        # "sentence-transformers/paraphrase-albert-small-v2",
     ],
     "bart": [
         "sshleifer/distilbart-cnn-6-6",
-        "facebook/bart-large-cnn"
+        "facebook/bart-large-cnn",
     ],
     "bert": [
         "bert-base-uncased",
@@ -19,46 +19,46 @@ TEST_DATA = {
         "bert-base-multilingual-uncased",
         "bert-base-multilingual-cased",
         "sentence-transformers/all-MiniLM-L6-v2",
-        "ckiplab/bert-base-chinese-ner"
+        "ckiplab/bert-base-chinese-ner",
     ],
     "clip": [
-        "openai/clip-vit-base-patch16"
+        "openai/clip-vit-base-patch16",
     ],
     "codegen": [
         "Salesforce/codegen-350M-mono",
         "Salesforce/codegen-350M-multi",
-        "Salesforce/codegen-350M-nl"
+        "Salesforce/codegen-350M-nl",
     ],
     "distilbert": [
         "distilbert-base-uncased",
         "distilbert-base-cased",
-        "Davlan/distilbert-base-multilingual-cased-ner-hrl"
+        "Davlan/distilbert-base-multilingual-cased-ner-hrl",
     ],
     "gpt-neo": [
-        "EleutherAI/gpt-neo-125M"
+        "EleutherAI/gpt-neo-125M",
     ],
     "gpt2": [
         "gpt2",
-        "distilgpt2"
+        "distilgpt2",
     ],
     "m2m_100": [
         "facebook/nllb-200-distilled-600M",
     ],
     "mobilebert": [
-        "google/mobilebert-uncased"
+        "google/mobilebert-uncased",
     ],
     "mt5": [
-        "google/mt5-small"
+        "google/mt5-small",
     ],
     "roberta": [
         "roberta-base",
         "distilroberta-base",
         "roberta-large-mnli",
-        "sentence-transformers/all-distilroberta-v1"
+        "sentence-transformers/all-distilroberta-v1",
     ],
     "squeezebert": [
         "squeezebert/squeezebert-uncased",
-        "squeezebert/squeezebert-mnli"
+        "squeezebert/squeezebert-mnli",
     ],
     "t5": [
         "t5-small",
@@ -66,15 +66,15 @@ TEST_DATA = {
         "google/t5-v1_1-small",
         "google/t5-v1_1-base",
         "google/flan-t5-small",
-        "google/flan-t5-base"
+        "google/flan-t5-base",
     ],
     "vision-encoder-decoder": [
-        "nlpconnect/vit-gpt2-image-captioning"
+        "nlpconnect/vit-gpt2-image-captioning",
     ],
     "whisper": [
         "openai/whisper-tiny",
-        "openai/whisper-tiny.en"
-    ]
+        "openai/whisper-tiny.en",
+    ],
 }
 
 texts_data = {
@@ -88,8 +88,7 @@ texts_data = {
         "This\n\nis\na\ntest.",
         "let a = obj.toString();\ntoString();",
     ],
-    "custom": {
-    }
+    "custom": {},
 }
 
 
@@ -102,29 +101,25 @@ def main():
         for tokenizer_name in tokenizer_names:
             # Load tokenizer
             tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-            print(f'{tokenizer=}')
 
             tokenizer_results = []
 
-            shared_texts = texts_data['shared']
-            custom_texts = texts_data['custom'].get(tokenizer_name, [])
+            shared_texts = texts_data["shared"]
+            custom_texts = texts_data["custom"].get(tokenizer_name, [])
 
             # Run tokenizer on test cases
             for text in shared_texts + custom_texts:
                 tokens = tokenizer(text).data
-                tokenizer_results.append({
-                    'input': text,
-                    'target': tokens
-                })
-                print(f'{tokens=}')
+                tokenizer_results.append({"input": text, "target": tokens})
 
             results[tokenizer_name] = tokenizer_results
 
-    results_file = os.path.join(os.path.dirname(
-        os.path.abspath(__file__)), 'tests.json')
-    with open(results_file, 'w', encoding='utf-8') as fp:
+    results_file = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "tests.json"
+    )
+    with open(results_file, "w", encoding="utf-8") as fp:
         json.dump(results, fp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
