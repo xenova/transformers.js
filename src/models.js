3d0cd78e83a3b82c616787a7fb418699cedb91e3
Joshua Lochner <admin@xenova.com>
2023-05-08 17:16:48 +0200

    Update model name in example

--- src/models.js
+++ src/models.js
@@ -26,8 +26,8 @@
  * ```javascript
  * import { AutoModelForSeq2SeqLM, AutoTokenizer } from '@xenova/transformers';
  * 
- * let tokenizer = await AutoTokenizer.from_pretrained('t5-small');
- * let model = await AutoModelForSeq2SeqLM.from_pretrained('t5-small');
+ * let tokenizer = await AutoTokenizer.from_pretrained('Xenova/t5-small');
+ * let model = await AutoModelForSeq2SeqLM.from_pretrained('Xenova/t5-small');
  *
  * let { input_ids } = await tokenizer('translate English to German: I love transformers!');
  * let outputs = await model.generate(input_ids);
