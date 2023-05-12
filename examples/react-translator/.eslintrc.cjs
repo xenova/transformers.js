d4cd9ded7642bfa9b58bd2a87aefd3bd36b773bf
Joshua Lochner <admin@xenova.com>
2023-05-12 03:24:04 +0200

    Update .eslintrc.cjs

--- examples/react-translator/.eslintrc.cjs
+++ examples/react-translator/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, 'node': true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -11,5 +11,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
   },
 }
