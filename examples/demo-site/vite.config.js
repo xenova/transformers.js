0b403ce8e4cf4ab447f8f3b208920aa4e77dc499
Joshua Lochner <admin@xenova.com>
2023-05-16 19:10:02 +0200

    Set base path for demo site from environment variable if present

--- examples/demo-site/vite.config.js
+++ examples/demo-site/vite.config.js
@@ -1,7 +1,11 @@
 import path from 'path';
 
+// Needed for deploying to GitHub pages
+const BASE_PATH = process.env.BASE_PATH ?? '';
+
 export default {
     // config options
+    base: BASE_PATH,
     root: path.join(__dirname, 'src'),
     build: {
         outDir: path.join(__dirname, 'dist')
