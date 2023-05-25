0a260c5e59da375e74e3f7d10d6710e22e36019e
Hans <admin@itsrv.tw>
2023-05-25 13:17:13 +0800

    Add missing var

--- src/backends/onnx.js
+++ src/backends/onnx.js
@@ -33,7 +33,7 @@ function setupRuntime(module) {
 }
 
 if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
-    import('onnxruntime-react-native').then(() => {
+    import('onnxruntime-react-native').then((ONNX_RN) => {
         setupRuntime(ONNX_RN);
         executionProviders.unshift('cpu');
     });
