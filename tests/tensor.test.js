71941829b442b81b4346aa7f17ab9b9df920de4b
Joshua Lochner <admin@xenova.com>
2023-07-06 19:45:12 +0200

    Add `cat` unit tests for > 2 tensors

--- tests/tensor.test.js
+++ tests/tensor.test.js
@@ -15,21 +15,36 @@ describe('Tensor operations', () => {
         it('should concatenate on dim=0', async () => {
             const t1 = new Tensor('float32', [1, 2, 3], [1, 3]);
             const t2 = new Tensor('float32', [4, 5, 6, 7, 8, 9], [2, 3]);
-            const t3 = new Tensor('float32', [1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3]);
+            const t3 = new Tensor('float32', [10, 11, 12], [1, 3]);
 
-            const concatenated = cat([t1, t2], 0);
-            compare(concatenated, t3, 1e-3);
+            const target1 = new Tensor('float32', [1, 2, 3, 4, 5, 6, 7, 8, 9], [3, 3]);
+            const target2 = new Tensor('float32', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [4, 3]);
+
+            // 2 tensors
+            const concatenated1 = cat([t1, t2], 0);
+            compare(concatenated1, target1, 1e-3);
+
+            // 3 tensors
+            const concatenated2 = cat([t1, t2, t3], 0);
+            compare(concatenated2, target2, 1e-3);
         });
 
 
         it('should concatenate on dim=1', async () => {
             const t1 = new Tensor('float32', [1, 2, 3, -1, -2, -3], [2, 3, 1]);
             const t2 = new Tensor('float32', [4, -4], [2, 1, 1]);
-            const t3 = new Tensor('float32', [1, 2, 3, 4, -1, -2, -3, -4], [2, 4, 1]);
+            const t3 = new Tensor('float32', [5, 6, -5, -6], [2, 2, 1]);
 
-            const concatenated = cat([t1, t2], 1);
-            compare(concatenated, t3, 1e-3);
+            const target1 = new Tensor('float32', [1, 2, 3, 4, -1, -2, -3, -4], [2, 4, 1]);
+            const target2 = new Tensor('float32', [1, 2, 3, 4, 5, 6, -1, -2, -3, -4, -5, -6], [2, 6, 1]);
 
+            // 2 tensors
+            const concatenated1 = cat([t1, t2], 1);
+            compare(concatenated1, target1, 1e-3);
+
+            // 3 tensors
+            const concatenated2 = cat([t1, t2, t3], 1);
+            compare(concatenated2, target2, 1e-3);
         });
     });
 });
