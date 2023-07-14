ac86b8006aafb1a46963691983007af885734b3c
Joshua Lochner <admin@xenova.com>
2023-07-15 00:37:56 +0200

    Update example extension

---
+++ examples/extension/src/content.js
@@ -0,0 +1,11 @@
+// content.js - the content scripts which is run in the context of web pages, and has access
+// to the DOM and other web APIs.
+
+// Example usage:
+// const message = {
+//     action: 'classify',
+//     text: 'text to classify',
+// }
+// chrome.runtime.sendMessage(message, (response) => {
+//     console.log('received user data', response)
+// });
