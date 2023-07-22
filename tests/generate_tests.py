a0ec3c29b8b595c4e3dc057638fdfbad8daaa491
Joshua Lochner <admin@xenova.com>
2023-07-22 03:23:24 +0200

    Add problematic string to unit tests

--- tests/generate_tests.py
+++ tests/generate_tests.py
@@ -39,6 +39,7 @@ TOKENIZER_TEST_DATA = {
         "test $1 R2 #3 €4 £5 ¥6 ₣7 ₹8 ₱9 test",
         "I bought an apple for $1.00 at the store.",
         "you…  ",
+        "\u0079\u006F\u0075\u2026\u00A0\u00A0",
     ],
     "custom": {
         "tiiuae/falcon-7b": [
