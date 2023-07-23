e4e44b32aa9d3d7fbd916daab9082eb85556f4b3
Joshua Lochner <admin@xenova.com>
2023-07-24 00:58:36 +0200

    Create example next app

---
+++ examples/next/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        // Ignore node-specific modules when bundling for the browser
+        // https://webpack.js.org/configuration/resolve/#resolvealias
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "sharp$": false,
+            "onnxruntime-node$": false,
+        }
+        return config;
+    },
+};
+
+module.exports = nextConfig;
