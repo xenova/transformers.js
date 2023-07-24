0e9e02dc0c347641449b87440c71ef6305728aad
Joshua Lochner <admin@xenova.com>
2023-07-24 02:59:03 +0200

    Update next configs

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Export as a static site
+    output: 'export',
+
+    // Override the default webpack configuration
     webpack: (config) => {
         // Ignore node-specific modules when bundling for the browser
         // https://webpack.js.org/configuration/resolve/#resolvealias
