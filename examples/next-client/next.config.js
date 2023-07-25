389cd453f3ae4bc784a9b75ebea9e3b8e18c18e6
Joshua Lochner <admin@xenova.com>
2023-07-25 21:35:14 +0200

    Update next.config.js files

--- examples/next-client/next.config.js
+++ examples/next-client/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Export as a static site
-    output: 'export',
+    // (Optional) Export as a static site
+    // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+    output: 'export', // Feel free to modify/remove this option
 
     // Override the default webpack configuration
     webpack: (config) => {
         // Ignore node-specific modules when bundling for the browser
-        // https://webpack.js.org/configuration/resolve/#resolvealias
+        // See https://webpack.js.org/configuration/resolve/#resolvealias
         config.resolve.alias = {
             ...config.resolve.alias,
             "sharp$": false,
--- examples/next-server/next.config.js
+++ examples/next-server/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // (Optional) Export as a standalone site
+    // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
+    output: 'standalone', // Feel free to modify/remove this option
+    
     // Indicate that these packages should not be bundled by webpack
     experimental: {
         serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
