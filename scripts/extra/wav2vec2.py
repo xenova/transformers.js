dd806694d9ae797db72c47f1f095fecdc099674c
Joshua Lochner <admin@xenova.com>
2023-07-29 09:55:14 +0200

    Improve generated wav2vec2 tokenizer json

--- scripts/extra/wav2vec2.py
+++ scripts/extra/wav2vec2.py
@@ -5,9 +5,34 @@ def generate_tokenizer_json(tokenizer):
         "version": "1.0",
         "truncation": None,
         "padding": None,
-        "added_tokens": [],
-        "normalizer": None,
-        "pre_tokenizer": None,
+        "added_tokens": [
+            {
+                "id": v,
+                "content": k,
+                "single_word": False,
+                "lstrip": False,
+                "rstrip": False,
+                "normalized": False,
+                "special": True
+            }
+            for k, v in tokenizer.vocab.items()
+            if k.startswith('<') and k.endswith('>')
+        ],
+        "normalizer": {
+            "type": "Replace",
+            "pattern": {
+                "String": " "
+            },
+            "content": "|"
+        },
+        "pre_tokenizer": {
+            "type": "Split",
+            "pattern": {
+                "Regex": ""
+            },
+            "behavior": "Isolated",
+            "invert": False
+        },
         "post_processor": None,
         "decoder": {
             "type": "CTC",
