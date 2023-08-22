5451123cd742968b6644f788041c5b1117837b0d
Joshua Lochner <admin@xenova.com>
2023-08-22 03:02:30 +0200

    Add example code for image-to-text pipeline

--- src/pipelines.js
+++ src/pipelines.js
@@ -1300,6 +1300,14 @@ export class AutomaticSpeechRecognitionPipeline extends Pipeline {
 
 /**
  * Image To Text pipeline using a `AutoModelForVision2Seq`. This pipeline predicts a caption for a given image.
+ * 
+ * **Example:** Generate a caption for an image w/ `Xenova/vit-gpt2-image-captioning`.
+ * ```javascript
+ * let url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/cats.jpg';
+ * let captioner = await pipeline('image-to-text', 'Xenova/vit-gpt2-image-captioning');
+ * let output = await captioner(url);
+ * // [{ generated_text: 'a cat laying on a couch with another cat' }]
+ * ```
  */
 export class ImageToTextPipeline extends Pipeline {
     /**
