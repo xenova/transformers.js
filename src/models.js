2cda7e4b6a9bc1901440b7a0cb528c132d0a03de
Joshua Lochner <admin@xenova.com>
2023-09-05 03:47:17 +0200

    Add support for XLM models

--- src/models.js
+++ src/models.js
@@ -1980,6 +1980,80 @@ export class RobertaForQuestionAnswering extends RobertaPreTrainedModel {
 }
 //////////////////////////////////////////////////
 
+
+//////////////////////////////////////////////////
+// XLM models
+/**
+ * An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained models.
+ */
+export class XLMPreTrainedModel extends PreTrainedModel { }
+
+/**
+ * The bare XLM Model transformer outputting raw hidden-states without any specific head on top.
+ */
+export class XLMModel extends XLMPreTrainedModel { }
+
+/**
+ * The XLM Model transformer with a language modeling head on top (linear layer with weights tied to the input embeddings).
+ */
+export class XLMWithLMHeadModel extends XLMPreTrainedModel {
+    /**
+     * Calls the model on new inputs.
+     *
+     * @param {Object} model_inputs The inputs to the model.
+     * @returns {Promise<MaskedLMOutput>} returned object
+     */
+    async _call(model_inputs) {
+        return new MaskedLMOutput(await super._call(model_inputs));
+    }
+}
+
+/**
+ * XLM Model with a sequence classification/regression head on top (a linear layer on top of the pooled output)
+ */
+export class XLMForSequenceClassification extends XLMPreTrainedModel {
+    /**
+     * Calls the model on new inputs.
+     *
+     * @param {Object} model_inputs The inputs to the model.
+     * @returns {Promise<SequenceClassifierOutput>} returned object
+     */
+    async _call(model_inputs) {
+        return new SequenceClassifierOutput(await super._call(model_inputs));
+    }
+}
+
+/**
+ * XLM Model with a token classification head on top (a linear layer on top of the hidden-states output)
+ */
+export class XLMForTokenClassification extends XLMPreTrainedModel {
+    /**
+     * Calls the model on new inputs.
+     *
+     * @param {Object} model_inputs The inputs to the model.
+     * @returns {Promise<TokenClassifierOutput>} An object containing the model's output logits for token classification.
+     */
+    async _call(model_inputs) {
+        return new TokenClassifierOutput(await super._call(model_inputs));
+    }
+}
+
+/**
+ * XLM Model with a span classification head on top for extractive question-answering tasks
+ */
+export class XLMForQuestionAnswering extends XLMPreTrainedModel {
+    /**
+     * Calls the model on new inputs.
+     *
+     * @param {Object} model_inputs The inputs to the model.
+     * @returns {Promise<QuestionAnsweringModelOutput>} returned object
+     */
+    async _call(model_inputs) {
+        return new QuestionAnsweringModelOutput(await super._call(model_inputs));
+    }
+}
+//////////////////////////////////////////////////
+
 //////////////////////////////////////////////////
 // XLMRoberta models
 export class XLMRobertaPreTrainedModel extends PreTrainedModel { }
@@ -3090,6 +3164,7 @@ const MODEL_MAPPING_NAMES_ENCODER_ONLY = new Map([
     ['albert', AlbertModel],
     ['distilbert', DistilBertModel],
     ['roberta', RobertaModel],
+    ['xlm', XLMModel],
     ['xlm-roberta', XLMRobertaModel],
     ['clip', CLIPModel],
     ['mobilebert', MobileBertModel],
@@ -3136,6 +3211,7 @@ const MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING_NAMES = new Map([
     ['albert', AlbertForSequenceClassification],
     ['distilbert', DistilBertForSequenceClassification],
     ['roberta', RobertaForSequenceClassification],
+    ['xlm', XLMForSequenceClassification],
     ['xlm-roberta', XLMRobertaForSequenceClassification],
     ['bart', BartForSequenceClassification],
     ['mobilebert', MobileBertForSequenceClassification],
@@ -3149,6 +3225,7 @@ const MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING_NAMES = new Map([
     ['mpnet', MPNetForTokenClassification],
     ['distilbert', DistilBertForTokenClassification],
     ['roberta', RobertaForTokenClassification],
+    ['xlm', XLMForTokenClassification],
     ['xlm-roberta', XLMRobertaForTokenClassification],
 ]);
 
@@ -3180,6 +3257,7 @@ const MODEL_FOR_MASKED_LM_MAPPING_NAMES = new Map([
     ['albert', AlbertForMaskedLM],
     ['distilbert', DistilBertForMaskedLM],
     ['roberta', RobertaForMaskedLM],
+    ['xlm', XLMWithLMHeadModel],
     ['xlm-roberta', XLMRobertaForMaskedLM],
     ['mobilebert', MobileBertForMaskedLM],
     ['squeezebert', SqueezeBertForMaskedLM],
@@ -3193,6 +3271,7 @@ const MODEL_FOR_QUESTION_ANSWERING_MAPPING_NAMES = new Map([
     ['albert', AlbertForQuestionAnswering],
     ['distilbert', DistilBertForQuestionAnswering],
     ['roberta', RobertaForQuestionAnswering],
+    ['xlm', XLMForQuestionAnswering],
     ['xlm-roberta', XLMRobertaForQuestionAnswering],
     ['mobilebert', MobileBertForQuestionAnswering],
     ['squeezebert', SqueezeBertForQuestionAnswering],
