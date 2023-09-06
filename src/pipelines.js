3ce23de56ecb2d2a704c07d278b11291f01f3f04
Joshua Lochner <admin@xenova.com>
2023-09-06 15:42:36 +0200

    Add example of multilingual translation with MBart models

--- src/pipelines.js
+++ src/pipelines.js
@@ -600,6 +600,19 @@ export class SummarizationPipeline extends Text2TextGenerationPipeline {
  * // [{ translation_text: 'Life is like a box of chocolate.' }]
  * ```
  * 
+ * **Example:** Multilingual translation w/ `Xenova/mbart-large-50-many-to-many-mmt`.
+ * 
+ * See [here](https://huggingface.co/facebook/mbart-large-50-many-to-many-mmt#languages-covered)
+ * for the full list of languages and their corresponding codes.
+ * 
+ * ```javascript
+ * let translator = await pipeline('translation', 'Xenova/mbart-large-50-many-to-many-mmt');
+ * let output = await translator('संयुक्त राष्ट्र के प्रमुख का कहना है कि सीरिया में कोई सैन्य समाधान नहीं है', {
+ *   src_lang: 'hi_IN', // Hindi
+ *   tgt_lang: 'fr_XX', // French
+ * });
+ * // [{ translation_text: 'Le chef des Nations affirme qu 'il n 'y a military solution in Syria.' }]
+ * ```
  */
 export class TranslationPipeline extends Text2TextGenerationPipeline {
     _key = 'translation_text';
