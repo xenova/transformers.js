d8610aa5f7416eaa3872442903caef4bbc5a22ad
Joshua Lochner <admin@xenova.com>
2023-09-11 18:34:45 +0200

    Cleanup

--- examples/javascript-object-detection/index.js
+++ examples/javascript-object-detection/index.js
@@ -3,16 +3,14 @@ import { pipeline, env } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers
 // Since we will be download the model from the Hugging Face model hub, we can skip the local model check
 env.allowLocalModels = false;
 
-import { generateRandomColor, getScaledCoordinates } from './utils.js'
-
-const statusParagraph = document.getElementById("status");
+const statusParagraph = document.getElementById('status');
 const fileUploadElement = document.getElementById('file-upload');
-const imageContainer = document.getElementById("image-container");
+const imageContainer = document.getElementById('image-container');
 
 // Start loading the model on page load
-statusParagraph.textContent = "Loading model...";
+statusParagraph.textContent = 'Loading model...';
 const detector = await pipeline('object-detection', 'Xenova/detr-resnet-50');
-statusParagraph.textContent = "Ready";
+statusParagraph.textContent = 'Ready';
 
 fileUploadElement.addEventListener('change', function (e) {
     const file = e.target.files[0];
@@ -22,51 +20,47 @@ fileUploadElement.addEventListener('change', function (e) {
 });
 
 function onFileReaderLoad(e) {
-    // Remove all the bounding boxes and labels from the previous image
-    document.querySelectorAll('.bounding-box-label, .bounding-box')
-        .forEach(element => element.remove());
-
-    const base64String = e.target.result;
-    const imageEl = document.createElement('img');
-    imageEl.src = base64String;
-    imageContainer.appendChild(imageEl);
-    runModel(imageEl);
+    imageContainer.innerHTML = '';
+    const image = document.createElement('img');
+    image.src = e.target.result;
+    imageContainer.appendChild(image);
+    runModel(image);
 };
 
-async function runModel(imageEl) {
-    statusParagraph.textContent = "Analysing ...";
-    const output = await detector(imageEl.src, { threshold: 0.5 });
-    statusParagraph.textContent = "";
-    output.forEach(object => {
-        renderBox(object, imageEl);
-    })
+async function runModel(img) {
+    statusParagraph.textContent = 'Analysing...';
+    const output = await detector(img.src, {
+        threshold: 0.5,
+        percentage: true, // Return in range [0, 1]
+    });
+    statusParagraph.textContent = '';
+    output.forEach(renderBox)
 }
 
-function renderBox(data, imageEl) {
+function renderBox(data) {
     const { box, label } = data;
-    const { xmax, xmin, ymax, ymin } = getScaledCoordinates(box, imageEl);
-    const color = generateRandomColor();
+    const { xmax, xmin, ymax, ymin } = box;
 
-    // Calculate the width and height of the bounding box
-    const boxWidth = xmax - xmin;
-    const boxHeight = ymax - ymin;
+    // Generate a random color for the box
+    const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, 0);
 
     // Draw the box
-    const boundingBoxElement = document.createElement("div");
-    boundingBoxElement.className = "bounding-box";
-    boundingBoxElement.style.border = `2px solid ${color}`;
-    boundingBoxElement.style.left = xmin + "px";
-    boundingBoxElement.style.top = ymin + "px";
-    boundingBoxElement.style.width = boxWidth + "px";
-    boundingBoxElement.style.height = boxHeight + "px";
-    imageContainer.appendChild(boundingBoxElement);
+    const boundingBoxElement = document.createElement('div');
+    boundingBoxElement.className = 'bounding-box';
+    Object.assign(boundingBoxElement.style, {
+        borderColor: color,
+        left: 100 * xmin + '%',
+        top: 100 * ymin + '%',
+        width: 100 * (xmax - xmin) + '%',
+        height: 100 * (ymax - ymin) + '%',
+    })
 
-    // Draw the label
-    let labelSpan = document.createElement('span');
+    // Draw label
+    const labelSpan = document.createElement('span');
     labelSpan.textContent = label;
-    labelSpan.className = "bounding-box-label";
+    labelSpan.className = 'bounding-box-label';
     labelSpan.style.backgroundColor = color;
-    labelSpan.style.left = xmin + "px";
-    labelSpan.style.top = (ymin - 12) + "px";
-    imageContainer.appendChild(labelSpan);
+
+    boundingBoxElement.appendChild(labelSpan);
+    imageContainer.appendChild(boundingBoxElement);
 }
