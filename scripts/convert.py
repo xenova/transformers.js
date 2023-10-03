08966a2623142e17d2bc90d409b4c52ea01e9b33
Joshua Lochner <admin@xenova.com>
2023-10-03 11:40:18 +0200

    Add tokenizer.json export for speecht5 models

--- scripts/convert.py
+++ scripts/convert.py
@@ -311,6 +311,13 @@ def main():
         # TODO allow user to specify vocoder path
         export_kwargs["model_kwargs"] = {"vocoder": "microsoft/speecht5_hifigan"}
 
+        if tokenizer is not None:
+            from .extra.speecht5 import generate_tokenizer_json
+            tokenizer_json = generate_tokenizer_json(tokenizer)
+
+            with open(os.path.join(output_model_folder, 'tokenizer.json'), 'w', encoding='utf-8') as fp:
+                json.dump(tokenizer_json, fp, indent=4)
+
     else:
         pass  # TODO
 
