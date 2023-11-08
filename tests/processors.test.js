a9adb6ab994c214804dee26c225778ef52b54aea
Joshua Lochner <admin@xenova.com>
2023-11-08 05:29:30 +0200

    Add SamProcessor test case

--- tests/processors.test.js
+++ tests/processors.test.js
@@ -7,12 +7,21 @@ import { compare } from './test_utils.js';
 env.allowLocalModels = false;
 env.useFSCache = false;
 
+const sum = (array) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; ++i) {
+        sum += array[i];
+    }
+    return sum;
+}
+
 describe('Processors', () => {
 
     describe('Image processors', () => {
 
         const models = [
             'caidas/swin2SR-classical-sr-x2-64',
+            'facebook/sam-vit-base',
         ];
         const TEST_IMAGES = {
             pattern_3x3: 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/pattern_3x3.png',
@@ -28,14 +37,26 @@ describe('Processors', () => {
                 const { pixel_values } = await processor(image);
 
                 compare(pixel_values.dims, [1, 3, 8, 8]);
-                compare(pixel_values.data.reduce((a, b) => a + b, 0), 104.80000066757202);
+                compare(sum(pixel_values.data), 104.80000066757202);
             }
 
             { // Do not pad if already a multiple of 8 (8x8 -> 8x8)
                 const image = await RawImage.fromURL(TEST_IMAGES.checkerboard_8x8);
                 const { pixel_values } = await processor(image);
                 compare(pixel_values.dims, [1, 3, 8, 8]);
-                compare(pixel_values.data.reduce((a, b) => a + b, 0), 96);
+                compare(sum(pixel_values.data), 96);
+            }
+        });
+
+        // do_pad=true, "pad_size": {"height": 1024,"width": 1024 },
+        it(models[1], async () => {
+            const processor = await AutoProcessor.from_pretrained(m(models[1]))
+
+            { // Basic test
+                const image = await RawImage.fromURL(TEST_IMAGES.pattern_3x3);
+                const { pixel_values } = await processor(image, [[[0, 0]]]);
+                compare(pixel_values.dims, [1, 3, 1024, 1024]);
+                compare(sum(pixel_values.data), -1417375.5943619595);
             }
         });
 
