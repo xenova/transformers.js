147dc7899a6a95d0f6c378734a644aec475218f2
Joshua Lochner <admin@xenova.com>
2023-12-16 17:30:33 +0200

    Add unit test for user-defined chat templates
    Example from https://discuss.huggingface.co/t/issue-with-llama-2-chat-template-and-out-of-date-documentation/61645/3

--- tests/tokenizers.test.js
+++ tests/tokenizers.test.js
@@ -100,6 +100,58 @@ describe('Chat templates', () => {
         compare(input_ids, [1, 733, 16289, 28793, 22557, 28725, 910, 460, 368, 28804, 733, 28748, 16289, 28793, 28737, 28742, 28719, 2548, 1598, 28723, 1602, 541, 315, 1316, 368, 3154, 28804, 2, 28705, 733, 16289, 28793, 315, 28742, 28715, 737, 298, 1347, 805, 910, 10706, 5752, 1077, 3791, 28808, 733, 28748, 16289, 28793])
     });
 
+    it('should support user-defined chat template', async () => {
+        const tokenizer = await AutoTokenizer.from_pretrained("Xenova/llama-tokenizer");
+
+        const chat = [
+            { role: 'user', content: 'Hello, how are you?' },
+            { role: 'assistant', content: "I'm doing great. How can I help you today?" },
+            { role: 'user', content: "I'd like to show off how chat templating works!" },
+        ]
+
+        // https://discuss.huggingface.co/t/issue-with-llama-2-chat-template-and-out-of-date-documentation/61645/3
+        const chat_template = (
+            "{% if messages[0]['role'] == 'system' %}" +
+            "{% set loop_messages = messages[1:] %}" +  // Extract system message if it's present
+            "{% set system_message = messages[0]['content'] %}" +
+            "{% elif USE_DEFAULT_PROMPT == true and not '<<SYS>>' in messages[0]['content'] %}" +
+            "{% set loop_messages = messages %}" +  // Or use the default system message if the flag is set
+            "{% set system_message = 'DEFAULT_SYSTEM_MESSAGE' %}" +
+            "{% else %}" +
+            "{% set loop_messages = messages %}" +
+            "{% set system_message = false %}" +
+            "{% endif %}" +
+            "{% if loop_messages|length == 0 and system_message %}" +  // Special handling when only sys message present
+            "{{ bos_token + '[INST] <<SYS>>\\n' + system_message + '\\n<</SYS>>\\n\\n [/INST]' }}" +
+            "{% endif %}" +
+            "{% for message in loop_messages %}" +  // Loop over all non-system messages
+            "{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}" +
+            "{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}" +
+            "{% endif %}" +
+            "{% if loop.index0 == 0 and system_message != false %}" +  // Embed system message in first message
+            "{% set content = '<<SYS>>\\n' + system_message + '\\n<</SYS>>\\n\\n' + message['content'] %}" +
+            "{% else %}" +
+            "{% set content = message['content'] %}" +
+            "{% endif %}" +
+            "{% if message['role'] == 'user' %}" +  // After all of that, handle messages/roles in a fairly normal way
+            "{{ bos_token + '[INST] ' + content.strip() + ' [/INST]' }}" +
+            "{% elif message['role'] == 'system' %}" +
+            "{{ '<<SYS>>\\n' + content.strip() + '\\n<</SYS>>\\n\\n' }}" +
+            "{% elif message['role'] == 'assistant' %}" +
+            "{{ ' '  + content.strip() + ' ' + eos_token }}" +
+            "{% endif %}" +
+            "{% endfor %}"
+        )
+            .replaceAll('USE_DEFAULT_PROMPT', true)
+            .replaceAll('DEFAULT_SYSTEM_MESSAGE', 'You are a helpful, respectful and honest assistant.');
+
+        const text = await tokenizer.apply_chat_template(chat, { tokenize: false, return_tensor: false, chat_template });
+
+        expect(text).toEqual("<s>[INST] <<SYS>>\nYou are a helpful, respectful and honest assistant.\n<</SYS>>\n\nHello, how are you? [/INST] I'm doing great. How can I help you today? </s><s>[INST] I'd like to show off how chat templating works! [/INST]");
+
+        // TODO: Add test for token_ids once bug in transformers is fixed.
+    });
+
     // Dynamically-generated tests
     for (const [tokenizerName, tests] of Object.entries(templates)) {
 
