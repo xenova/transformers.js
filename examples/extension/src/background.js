4d9329730e3f2550db87cc754bd9756723b41fab
Brennan Barker <BrennanBarker@users.noreply.github.com>
2024-01-02 22:38:16 -0500

    perform inference in sidepanel vice background

--- examples/extension/src/background.js
+++ examples/extension/src/background.js
@@ -4,44 +4,6 @@ chrome.sidePanel
     .setPanelBehavior({ openPanelOnActionClick: true })
     .catch((error) => console.error(error));
 
-import { pipeline, env } from '@xenova/transformers';
-
-// Skip initial check for local models, since we are not loading any local models.
-env.allowLocalModels = false;
-
-// Due to a bug in onnxruntime-web, we must disable multithreading for now.
-// See https://github.com/microsoft/onnxruntime/issues/14445 for more information.
-env.backends.onnx.wasm.numThreads = 1;
-
-
-class PipelineSingleton {
-    static task = 'text-classification';
-    static model = 'Xenova/distilbert-base-uncased-finetuned-sst-2-english';
-    static instance = null;
-
-    static async getInstance(progress_callback = null) {
-        if (this.instance === null) {
-            this.instance = pipeline(this.task, this.model, { progress_callback });
-        }
-
-        return this.instance;
-    }
-}
-
-// Create generic classify function, which will be reused for the different types of events.
-const classify = async (text) => {
-    // Get the pipeline instance. This will load and build the model when run for the first time.
-    let model = await PipelineSingleton.getInstance((data) => {
-        // You can track the progress of the pipeline creation here.
-        // e.g., you can send `data` back to the UI to indicate a progress bar
-        // console.log('progress', data)
-    });
-
-    // Actually run the model on the input text
-    let result = await model(text);
-    return result;
-};
-
 ////////////////////// 1. Context Menus //////////////////////
 //
 // Add a listener to create the initial context menu items,
@@ -55,46 +17,23 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 });
 
+// add connection when sidepanel opens
+var connection
+chrome.runtime.onConnect.addListener(x => {
+    connection = x
+})
+
 // Perform inference when the user clicks a context menu
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     // Ignore context menu clicks that are not for classifications (or when there is no input)
     if (info.menuItemId !== 'classify-selection' || !info.selectionText) return;
 
-    // Perform classification on the selected text
-    let result = await classify(info.selectionText);
-
-    // Do something with the result
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },    // Run in the tab that the user clicked in
-        args: [result],               // The arguments to pass to the function
-        function: (result) => {       // The function to run
-            // NOTE: This function is run in the context of the web page, meaning that `document` is available.
-            console.log('result', result)
-            console.log('document', document)
-        },
-    });
+    if (connection) {  // sidepanel is open, post text for inference
+        console.log('found open connection')
+        connection.postMessage({ text: info.selectionText })
+    } else { // sidepanel not yet open; open sidepanel and persist text in session storage until panel can pick it up.
+        console.log('no connection found')
+        chrome.sidePanel.open({ windowId: tab.windowId }).catch(() => { })
+        chrome.storage.session.set({ 'inference_input': info.selectionText })
+    }
 });
-//////////////////////////////////////////////////////////////
-
-////////////////////// 2. Message Events /////////////////////
-// 
-// Listen for messages from the UI, process it, and send the result back.
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('sender', sender)
-    if (message.action !== 'classify') return; // Ignore messages that are not meant for classification.
-
-    // Run model prediction asynchronously
-    (async function () {
-        // Perform classification
-        let result = await classify(message.text);
-
-        // Send response back to UI
-        sendResponse(result);
-    })();
-
-    // return true to indicate we will send a response asynchronously
-    // see https://stackoverflow.com/a/46628145 for more information
-    return true;
-});
-//////////////////////////////////////////////////////////////
-
--- examples/extension/src/sidepanel.js
+++ examples/extension/src/sidepanel.js
@@ -1,29 +1,15 @@
-// popup.js - handles interaction with the extension's popup, sends requests to the
+// sidepanel.js - handles interaction with the extension's popup, sends requests to the
 // service worker (background.js), and updates the popup's UI (popup.html) on completion.
 
 const inputElement = document.getElementById('text');
 const outputElement = document.getElementById('output');
 
-// Listen for changes made to the textbox.
-inputElement.addEventListener('input', (event) => {
-
-    // Bundle the input data into a message.
-    const message = {
-        action: 'classify',
-        text: event.target.value,
-    }
-
-    // Send this message to the service worker.
-    chrome.runtime.sendMessage(message, (response) => {
-        // Handle results returned by the service worker (`background.js`) and update the popup's UI.
-        outputElement.innerText = JSON.stringify(response, null, 2);
-    });
-});
 
 import { pipeline, env } from '@xenova/transformers';
 
 // Skip initial check for local models, since we are not loading any local models.
 env.allowLocalModels = false;
+env.backends.onnx.wasm.numThreads = 1;
 
 
 class PipelineSingleton {
@@ -53,3 +39,62 @@ const classify = async (text) => {
     let result = await model(text);
     return result;
 };
+
+
+const main = async inference_input => {
+    inputElement.value = inference_input;
+    const result = await classify(inference_input)
+    outputElement.innerText = JSON.stringify(result, null, 2);
+}
+
+// Listen for changes made to the textbox.
+inputElement.addEventListener('input', async (event) => {
+    const result = await classify(event.target.value)
+    outputElement.innerText = JSON.stringify(result, null, 2);
+});
+
+const pending = await chrome.storage.session.get('inference_input')
+if (pending.inference_input?.length > 0) {
+    main(pending.inference_input)
+    chrome.storage.session.remove('inference_input')
+}
+const port = chrome.runtime.connect()
+port.onMessage.addListener((message, sender, sendResponse) => {
+    main(message.text)
+})
+
+
+// Listen for input incoming from the other pages (like a background service worker managing context menus)
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     console.log('sender', sender)
+
+//     // if (message.action !== 'classify') return; // Ignore messages that are not meant for classification.
+
+//     switch (message.action) {
+//         case 'classify':
+//         // inputElement.value = message.text // Update sidepanel UI with incoming text
+//         // classify(message.text)
+//         //     .then(result => {
+//         //         outputElement.innerText = JSON.stringify(result, null, 2); // Update sidepanel UI with output
+//         //         sendResponse(result)
+//         //     })
+//         // return true
+
+
+//     }
+
+//     // Run model prediction asynchronously
+//     // (async function () {
+//     //     // Perform classification
+//     //     let result = await classify(message.text);
+
+
+//     //     // Send response back to UI
+//     //     sendResponse(result);
+//     //     return true
+//     // })();
+
+//     // return true to indicate we will send a response asynchronously
+//     // see https://stackoverflow.com/a/46628145 for more information
+//     // return true;
+// });
