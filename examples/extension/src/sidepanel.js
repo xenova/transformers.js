1fb1290ea12ea844f5c8db21f89c3154d26cdfa3
Brennan Barker <BrennanBarker@users.noreply.github.com>
2024-01-02 23:21:08 -0500

    refactor and improve comments

--- examples/extension/src/sidepanel.js
+++ examples/extension/src/sidepanel.js
@@ -5,24 +5,27 @@ import { classify } from "./inference.mjs";
 const inputElement = document.getElementById('text');
 const outputElement = document.getElementById('output');
 
+const port = chrome.runtime.connect()
+
 const main = async inference_input => {
     inputElement.value = inference_input;
     const result = await classify(inference_input)
     outputElement.innerText = JSON.stringify(result, null, 2);
 }
 
-// Listen for changes made to the textbox.
-inputElement.addEventListener('input', async (event) => {
-    const result = await classify(event.target.value)
-    outputElement.innerText = JSON.stringify(result, null, 2);
-});
-
+// On load, check if there's any pending input saved by the background worker because the sidepanel wasn't open yet
 const pending = await chrome.storage.session.get('inference_input')
 if (pending.inference_input?.length > 0) {
     main(pending.inference_input)
     chrome.storage.session.remove('inference_input')
 }
-const port = chrome.runtime.connect()
+
+// Listen for additional input from the background worker once open
 port.onMessage.addListener((message, sender, sendResponse) => {
     main(message.text)
 })
+
+// Accept input added directly to the text box in the sidepanel UI
+inputElement.addEventListener('input', async (event) => {
+    main(event.target.value)
+});
