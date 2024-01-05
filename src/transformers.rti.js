97dd7af63ed182a474efaeb45e3009e23bc1a7d1
kungfooman <lama12345@gmail.com>
2024-01-05 12:37:14 +0100

    Update to @runtime-type-inspector/plugin-webpack5@3.0.0

--- src/transformers.rti.js
+++ src/transformers.rti.js
@@ -1,10 +1,10 @@
-import { typecheckOptions } from '@runtime-type-inspector/runtime';
+import { customTypes } from '@runtime-type-inspector/runtime';
 import { ONNX } from './backends/onnx.js';
 /**
  * @param {*} value 
  * @returns {boolean}
  */
-typecheckOptions.customTypes.ONNXTensor = value => {
+customTypes.ONNXTensor = value => {
   return value instanceof ONNX.Tensor;
 }
 export * from './transformers.js';
