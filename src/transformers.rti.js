55e150dfd92d2dcc6982798c961a8a3595801635
kungfooman <lama12345@gmail.com>
2024-01-25 17:20:46 +0100

    Update to @runtime-type-inspector/plugin-webpack5@3.2.0

--- src/transformers.rti.js
+++ src/transformers.rti.js
@@ -1,5 +1,6 @@
 import { customTypes } from '@runtime-type-inspector/runtime';
 import { ONNX } from './backends/onnx.js';
+import 'display-anything/src/style.js';
 /**
  * @param {*} value 
  * @returns {boolean}
