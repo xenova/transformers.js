b1f96a2fc967807e29afdc75be778a7cbadaa51b
Joshua Lochner <admin@xenova.com>
2024-01-27 17:50:03 +0200

    Add support for wav2vec2-bert (#544)
    * Add povey window function
    
    * Add `SeamlessM4TFeatureExtractor`
    
    * Add support for wav2vec2-bert models
    
    * Add `SeamlessM4TFeatureExtractor` processor unit tests
    
    * Add pipeline support for `wav2vec2-bert` models
    
    * Update JSDoc

--- scripts/convert.py
+++ scripts/convert.py
@@ -93,6 +93,7 @@ MODEL_SPECIFIC_QUANTIZE_PARAMS = {
 
 MODELS_WITHOUT_TOKENIZERS = [
     'wav2vec2',
+    'wav2vec2-bert',
     'wavlm',
     'hubert',
 ]
@@ -331,7 +332,7 @@ def main():
                 **get_main_export_kwargs(config, "automatic-speech-recognition")
             )
 
-    elif config.model_type in ('wav2vec2', 'hubert'):
+    elif config.model_type in ('wav2vec2', 'wav2vec2-bert', 'hubert'):
         if tokenizer is not None:
             from .extra.wav2vec2 import generate_tokenizer_json
             tokenizer_json = generate_tokenizer_json(tokenizer)
