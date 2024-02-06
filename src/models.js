99370eddae94b439b2c32c81a22ffdb397931395
Joshua Lochner <admin@xenova.com>
2024-02-06 12:39:26 +0200

    Fix typo

--- src/models.js
+++ src/models.js
@@ -3582,7 +3582,7 @@ export class Qwen2PreTrainedModel extends PreTrainedModel {
     }
 }
 /**
- * The bare LLaMA Model outputting raw hidden-states without any specific head on top.
+ * The bare Qwen2 Model outputting raw hidden-states without any specific head on top.
  */
 export class Qwen2Model extends Qwen2PreTrainedModel { }
 
