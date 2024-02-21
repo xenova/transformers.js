fc16dc72980f90e30bcb46a4cae9c3c75f206ef0
Joshua Lochner <admin@xenova.com>
2024-02-21 15:34:14 +0200

    Add Gemma tokenizer unit test

--- tests/generate_tests.py
+++ tests/generate_tests.py
@@ -36,6 +36,9 @@ ADDITIONAL_TOKENIZERS_TO_TEST = {
         # Uses a pretokenizer regex which is not compatible with JavaScript.
         'Qwen/Qwen1.5-0.5B-Chat',
     ],
+    'gemma': [
+        'hf-internal-testing/dummy-gemma',
+    ],
 }
 
 MODELS_TO_IGNORE = [
