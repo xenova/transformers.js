a68564dcb5769309084ad19b37e0a524e0c58e0a
Th3G33k <4394090+Th3G33k@users.noreply.github.com>
2024-05-10 02:53:44 -1000

    Beautify code

--- src/backends/onnx.js
+++ src/backends/onnx.js
@@ -72,16 +72,16 @@ export function deviceToExecutionProviders(device) {
  */
 export async function createInferenceSession(buffer, session_options) {
     // fix wasm relative path
-    if(apis.IS_BROWSER_ENV){
+    if (apis.IS_BROWSER_ENV) {
         let url = env.backends.onnx.wasm.wasmPaths;
-        if(!url.startsWith('http')){
-            env.backends.onnx.wasm.wasmPaths = url[0] == '/' ? location.origin + url : location.href.replace(/[^\/]+$/,'') + url;
+        if (!url.startsWith('http')) {
+            env.backends.onnx.wasm.wasmPaths = url[0] == '/' ? location.origin + url : location.href.replace(/[^\/]+$/, '') + url;
         }
     }
 
     // set log level
     let logLevel = ['verbose', 'info', 'warning', 'error', 'fatal'].indexOf(env.backends.onnx.logLevel);
-    if(logLevel >= 0){
+    if (logLevel >= 0) {
         session_options.logVerbosityLevel = session_options.logSeverityLevel = logLevel;
     }
 
