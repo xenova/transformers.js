75a1109be5cc7045db6508cbec27ea426bb5558e
Th3G33k <4394090+Th3G33k@users.noreply.github.com>
2024-05-11 09:23:38 -1000

    remove export onnx

--- src/backends/onnx.js
+++ src/backends/onnx.js
@@ -30,7 +30,7 @@ const supportedExecutionProviders = [];
 
 /** @type {import('../utils/devices.js').DeviceType[]} */
 let defaultExecutionProviders;
-export let ONNX;
+let ONNX;
 if (apis.IS_NODE_ENV) {
     ONNX = ONNX_NODE.default ?? ONNX_NODE;
     supportedExecutionProviders.push('cpu');
