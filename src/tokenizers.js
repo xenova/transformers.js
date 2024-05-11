56f0b922b4a81d7310d408e3437444b588074feb
Th3G33k <4394090+Th3G33k@users.noreply.github.com>
2024-05-11 13:11:27 -1000

    Update tokenizers.js

--- src/tokenizers.js
+++ src/tokenizers.js
@@ -3117,7 +3117,7 @@ export class PreTrainedTokenizer extends Callable {
         }
         else {
             if (!Array.isArray(batchEncoding)) batchEncoding = [batchEncoding];
-            if (!Array.isArray(batchEncoding[0])) {
+            if (typeof batchEncoding[0] == 'string') {
                 batchEncoding.forEach((val, key) =>{
                     batchEncoding[key] = val.split(' ');
                 })
