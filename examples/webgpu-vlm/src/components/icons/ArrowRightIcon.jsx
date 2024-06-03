046b292ac50a0de594d9916bbe2f9b1ffcbbd752
Joshua Lochner <admin@xenova.com>
2024-06-03 16:08:15 +0200

    Upload source code for WebGPU-VLM demo

---
+++ examples/webgpu-vlm/src/components/icons/ArrowRightIcon.jsx
@@ -0,0 +1,19 @@
+export default function ArrowRightIcon(props) {
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M5 12h14" />
+            <path d="m12 5 7 7-7 7" />
+        </svg>
+    )
+}
