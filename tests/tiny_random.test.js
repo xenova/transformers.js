939920d22f4e3af63fa3c25efec9378a97973cae
Joshua Lochner <admin@xenova.com>
2024-08-07 15:54:26 +0200

    Add gemma2 generation test

--- tests/tiny_random.test.js
+++ tests/tiny_random.test.js
@@ -27,6 +27,7 @@ import {
     CohereModel,
     CohereForCausalLM,
     GemmaForCausalLM,
+    Gemma2ForCausalLM,
     OPTForCausalLM,
     GPTNeoXForCausalLM,
     GPTJForCausalLM,
@@ -973,6 +974,51 @@ describe('Tiny random models', () => {
                 expect(outputs.tolist()).toEqual([
                     [0n, 2n, 17534n, 254059n, 254059n, 254059n, 254059n, 254059n, 254059n, 254059n],
                     [2n, 17534n, 2134n, 71055n, 71055n, 71055n, 71055n, 71055n, 71055n, 71055n]
+                ]);
+            }, MAX_TEST_EXECUTION_TIME);
+
+            afterAll(async () => {
+                await model?.dispose();
+            }, MAX_MODEL_DISPOSE_TIME);
+        });
+    });
+
+    describe('gemma', () => {
+        describe('Gemma2ForCausalLM', () => {
+            const model_id = 'hf-internal-testing/tiny-random-Gemma2ForCausalLM';
+            /** @type {Gemma2ForCausalLM} */
+            let model;
+            /** @type {GemmaTokenizer} */
+            let tokenizer;
+            beforeAll(async () => {
+                model = await Gemma2ForCausalLM.from_pretrained(model_id, {
+                    // TODO move to config
+                    ...DEFAULT_MODEL_OPTIONS,
+                });
+                tokenizer = await GemmaTokenizer.from_pretrained(model_id);
+                tokenizer.padding_side = 'left';
+            }, MAX_MODEL_LOAD_TIME);
+
+            it('batch_size=1', async () => {
+                const inputs = tokenizer('hello');
+                const outputs = await model.generate({
+                    ...inputs,
+                    max_length: 10,
+                });
+                expect(outputs.tolist()).toEqual([
+                    [2n, 17534n, 127534n, 160055n, 160055n, 160055n, 160055n, 160055n, 160055n, 160055n]
+                ]);
+            }, MAX_TEST_EXECUTION_TIME);
+
+            it('batch_size>1', async () => {
+                const inputs = tokenizer(['hello', 'hello world'], { padding: true });
+                const outputs = await model.generate({
+                    ...inputs,
+                    max_length: 10,
+                });
+                expect(outputs.tolist()).toEqual([
+                    [0n, 2n, 17534n, 127534n, 127534n, 215341n, 215341n, 215341n, 215341n, 215341n],
+                    [2n, 17534n, 2134n, 107508n, 160055n, 160055n, 160055n, 160055n, 160055n, 160055n]
                 ]);
             }, MAX_TEST_EXECUTION_TIME);
 
