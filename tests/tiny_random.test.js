45d1526ef9e4ebc5437b81a5310cad42b810d21e
Joshua Lochner <admin@xenova.com>
2024-08-27 18:46:27 +0200

    Add marian unit test

--- tests/tiny_random.test.js
+++ tests/tiny_random.test.js
@@ -63,6 +63,8 @@ import {
     // Other
     full,
     RawImage,
+    MarianMTModel,
+    MarianTokenizer,
 } from '@huggingface/transformers';
 
 import { init } from './init.js';
@@ -360,6 +362,52 @@ describe('Tiny random models', () => {
                 expect(outputs.tolist()).toEqual([
                     [0n, 0n, 0n, 0n, 0n, 0n, 0n, 0n, 0n, 0n],
                     [0n, 0n, 0n, 0n, 0n, 0n, 0n, 0n, 0n, 0n]
+                ]);
+            }, MAX_TEST_EXECUTION_TIME);
+
+            afterAll(async () => {
+                await model?.dispose();
+            }, MAX_MODEL_DISPOSE_TIME);
+        });
+    });
+
+    describe('marian', () => {
+
+        describe('MarianMTModel', () => {
+            const model_id = 'onnx-community/tiny-random-MarianMTModel';
+
+            /** @type {MarianMTModel} */
+            let model;
+            /** @type {MarianTokenizer} */
+            let tokenizer;
+            beforeAll(async () => {
+                model = await MarianMTModel.from_pretrained(model_id, {
+                    // TODO move to config
+                    ...DEFAULT_MODEL_OPTIONS,
+                });
+                tokenizer = await MarianTokenizer.from_pretrained(model_id);
+            }, MAX_MODEL_LOAD_TIME);
+
+            it('batch_size=1', async () => {
+                const inputs = tokenizer('hello');
+                const outputs = await model.generate({
+                    ...inputs,
+                    max_length: 10,
+                });
+                expect(outputs.tolist()).toEqual([
+                    [3n, 40672n, 8358n, 32810n, 32810n, 32810n, 32810n, 35687n, 33073n, 6870n],
+                ]);
+            }, MAX_TEST_EXECUTION_TIME);
+
+            it('batch_size>1', async () => {
+                const inputs = tokenizer(['hello', 'hello world'], { padding: true });
+                const outputs = await model.generate({
+                    ...inputs,
+                    max_length: 10,
+                });
+                expect(outputs.tolist()).toEqual([
+                    [3n, 40672n, 8358n, 32810n, 32810n, 32810n, 32810n, 35687n, 33073n, 6870n],
+                    [3n, 40672n, 8358n, 32810n, 32810n, 32810n, 32810n, 35687n, 33073n, 6870n],
                 ]);
             }, MAX_TEST_EXECUTION_TIME);
 
