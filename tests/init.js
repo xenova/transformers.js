0d3cd309a6a723e0afa5949cccd4f34f7ca5395b
Joshua Lochner <admin@xenova.com>
2024-09-10 10:16:07 +0000

    Run unit tests in parallel

--- tests/init.js
+++ tests/init.js
@@ -58,5 +58,5 @@ export function init() {
 }
 
 export const MAX_MODEL_LOAD_TIME = 10_000; // 10 seconds
-export const MAX_TEST_EXECUTION_TIME = 10_000; // 10 seconds
+export const MAX_TEST_EXECUTION_TIME = 30_000; // 30 seconds
 export const MAX_MODEL_DISPOSE_TIME = 1_000; // 1 second
