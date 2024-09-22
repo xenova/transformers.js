4a31e549d4452193a21f7adfc46bb407172a8bd2
Joshua Lochner <admin@xenova.com>
2024-09-22 02:00:13 +0000

    Add gemma tokenizer tests

--- tests/models/all_tokenization_tests.js
+++ tests/models/all_tokenization_tests.js
@@ -2,6 +2,7 @@ export * as BertTokenizer from "./bert/tokenization.js";
 export * as GPT2Tokenizer from "./gpt2/tokenization.js";
 export * as T5Tokenizer from "./t5/tokenization.js";
 export * as LlamaTokenizer from "./llama/tokenization.js";
+export * as GemmaTokenizer from "./gemma/tokenization.js";
 export * as WhisperTokenizer from "./whisper/tokenization.js";
 export * as FalconTokenizer from "./falcon/tokenization.js";
 export * as EsmTokenizer from "./esm/tokenization.js";
