b51886644c3f59d1cf5ffc1555566dfa36d14a3b
Kalleby Santos <105971119+kallebysantos@users.noreply.github.com>
2024-09-30 17:28:42 +0100

    Support to load ONNX APIs based on JS runtime (#947)
    * Add support for global `onnxruntime`
    
    - Allows custom JS runtimes to expose their own ONNX API.
    
    * Update onnx.js
    
    ---------
    
    Co-authored-by: Joshua Lochner <admin@xenova.com>

--- src/backends/onnx.js
+++ src/backends/onnx.js
@@ -59,7 +59,13 @@ const supportedDevices = [];
 /** @type {ONNXExecutionProviders[]} */
 let defaultDevices;
 let ONNX;
-if (apis.IS_NODE_ENV) {
+const ORT_SYMBOL = Symbol.for('onnxruntime');
+
+if (ORT_SYMBOL in globalThis) {
+  // If the JS runtime exposes their own ONNX runtime, use it
+  ONNX = globalThis[ORT_SYMBOL];
+
+} else if (apis.IS_NODE_ENV) {
     ONNX = ONNX_NODE.default ?? ONNX_NODE;
 
     // Updated as of ONNX Runtime 1.18.0
