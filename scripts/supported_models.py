4c88295a0723d3bb264a3bd6a34250cd0c5d5c75
sileod <damien.sileo@gmail.com>
2024-10-02 15:07:29 +0200

    Update supported_models.py

--- scripts/supported_models.py
+++ scripts/supported_models.py
@@ -297,7 +297,7 @@ SUPPORTED_MODELS = {
             'MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7',
             'sileod/deberta-v3-base-tasksource-nli',
             'sileod/deberta-v3-large-tasksource-nli',
-            'tasksource/deberta-small-long-nli'
+            'tasksource/deberta-small-long-nli',
             'tasksource/deberta-base-long-nli',
         ],
     },
