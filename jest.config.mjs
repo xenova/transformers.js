52c4ce7045f16fa0eeb37eecdfb1c0fafd2f6fe7
Joshua Lochner <admin@xenova.com>
2024-10-07 13:09:53 +0000

    Enable unit test coverage reports

--- jest.config.mjs
+++ jest.config.mjs
@@ -14,7 +14,7 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
@@ -23,9 +23,10 @@ export default {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "tests",
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
